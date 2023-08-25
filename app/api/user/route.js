85ead05bfb26065b92eb358c08db184d3993a928
alvropena <alvpe26@gmail.com>
2023-08-25 22:26:32 +0700

    update

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -11,7 +11,7 @@ async function handler(req, res) {
     try {
       const newUser = await prisma.user.create({
         data: {
-          id: parseInt(id),
+          id: id,
           email : email,
           name: name,
         }
